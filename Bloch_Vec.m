2584c5a694ecef368c07885f1cc49fdbef2dfdbb
David Wise <wisedavid@mac.com>
2015-11-22 20:54:41 +0000

    Finished updating to select individual datasets - note double requirment

--- Bloch_Vec.m
+++ Bloch_Vec.m
@@ -12,49 +12,65 @@ data_values = mat2dataset(Data.data);
 
 size(data_values);
 %Select the appropriate data for each bloch vector value
-xdata = data_values(1:20000, [1,2])
-zdata = data_values(20001:40000, [1,2])
-ydata = data_values(1:40000, [1,3])
+xdata = double(data_values(1:20000, [1,2]));
+zdata = double(data_values(20001:40000, [1,2]));
+ydata = double(data_values(1:40000, [1,3]));
 
 hold on
 
 plot(col_1,col_3)
 
 % Apply a Gaussian fit to both sets of data
-fun_x = fit(col_1,col_2,'gauss1')
-plot(fun_x)
-fun_x_2 = fit(col_1, col_3,'gauss1')
-plot(fun_x_2)
+% fun_x = fit(col_1,col_2,'gauss1')
+% plot(fun_x)
+% fun_x_2 = fit(col_1, col_3,'gauss1')
+% plot(fun_x_2)
+
+% Apply a Gaussian fit to the three data sets corresponding to Bloch
+% coefficients x, y, z
+
+
+x_fun = fit(xdata(:,1), xdata(:,2), 'gauss1')
+z_fun = fit(zdata(:,1), zdata(:,2), 'gauss1')
+y_fun = fit(ydata(:,1), ydata(:,2), 'gauss1')
+
+plot(x_fun)
+plot(z_fun)
+plot(y_fun)
+
 
 %Extract the coefficients from the Gaussians
-xd_coeffs_1 = coeffvalues(fun_x)
-xd_coeffs_2 = coeffvalues(fun_x_2)
+% xd_coeffs_1 = coeffvalues(fun_x)
+% xd_coeffs_2 = coeffvalues(fun_x_2)
+
+x_coeffs = coeffvalues(x_fun)
+z_coeffs = coeffvalues(z_fun)
+y_coeffs = coeffvalues(y_fun)
+
 %Division by root 2 is due to different matlab definition of the Gaussian
 %function
-if xd_coeffs_1(2) < 20000
-    amp_x = xd_coeffs_1(1)
-    mean_x = xd_coeffs_1(2)
-    sd_x = xd_coeffs_1(3)/(sqrt(2))
-    amp_z = xd_coeffs_1(4)
-    mean_z = xd_coeffs_1(5)
-    sd_z = xd_coeffs_1(6)/(sqrt(2))
-elseif xd_coeffs_1(2) > 20000
-    amp_z = xd_coeffs_1(1)
-    mean_z = xd_coeffs_1(2)
-    sd_z = xd_coeffs_1(3)/(sqrt(2))
-    amp_x = xd_coeffs_1(4)
-    mean_x = xd_coeffs_1(5)
-    sd_x = xd_coeffs_1(6)/(sqrt(2))
-end
+
+amp_x = x_coeffs(1)
+mean_x = x_coeffs(2)
+sd_x = x_coeffs(3)/(sqrt(2))
+
+amp_z = z_coeffs(1)
+mean_z = z_coeffs(2)
+sd_z = z_coeffs(3)/(sqrt(2))
+
+amp_y = y_coeffs(1)
+mean_y = y_coeffs(2)
+sd_y = y_coeffs(3)/(sqrt(2))
+
 % amp_x = xd_coeffs_1(1)
 % mean_x = xd_coeffs_1(2)
 % sd_x = xd_coeffs_1(3)/(sqrt(2))
 % amp_z = xd_coeffs_1(4)
 % mean_z = xd_coeffs_1(5)
 % sd_z = xd_coeffs_1(6)/(sqrt(2))
-amp_y = xd_coeffs_2(1)
-mean_y = xd_coeffs_2(2)
-sd_y = xd_coeffs_2(3)/(sqrt(2))
+% amp_y = xd_coeffs_2(1)
+% mean_y = xd_coeffs_2(2)
+% sd_y = xd_coeffs_2(3)/(sqrt(2))
 
 
 
